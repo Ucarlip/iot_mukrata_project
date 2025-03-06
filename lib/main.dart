b0644e15444d291df7ba5035f80288ea6ed04cbf
Ucarlip <modkak98@gmail.com>
2025-03-06 14:36:22 +0700

    Splashscreen Home Page and All setup

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:iot_mukrata_project/views/splash_screen_ui.dart';
+
+void main() {
+  runApp(const IotMukrataProject());
+}
+
+class IotMukrataProject extends StatefulWidget {
+  const IotMukrataProject({super.key});
+
+  @override
+  State<IotMukrataProject> createState() => _IotMukrataProjectState();
+}
+
+class _IotMukrataProjectState extends State<IotMukrataProject> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenUi(),
+    );
+  }
+}
